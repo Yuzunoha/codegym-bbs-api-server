c1c73c51e99dc5bdd3cdcc280d04e76a28d211eb
Yuzunoha <studysteadystandby@gmail.com>
2021-10-10 22:50:30 +0900

    登録ができた

--- html/laravelapp/app/Http/Controllers/AuthController.php
+++ html/laravelapp/app/Http/Controllers/AuthController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AuthRegisterPost;
+use App\Models\User;
+use App\Services\UtilService;
+
 class AuthController extends Controller
 {
     public function login()
@@ -9,8 +13,13 @@ class AuthController extends Controller
         return 'loginです';
     }
 
-    public function register()
+    public function register(AuthRegisterPost $request)
     {
-        return 'registerです';
+        /* emailチェック */
+        $email = $request->email;
+        if (User::where('email', $email)->first()) {
+            UtilService::throwHttpResponseException("email ${email} は既に登録されています。");
+        }
+        return User::create($request->toArray());
     }
 }
