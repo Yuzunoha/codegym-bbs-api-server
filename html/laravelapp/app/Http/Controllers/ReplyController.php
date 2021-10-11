22ffa0eaf4e8232b6b0efe2ec616e9c5189bc8c9
Yuzunoha <studysteadystandby@gmail.com>
2021-10-12 00:37:57 +0900

    ファットコントローラに向かう

--- html/laravelapp/app/Http/Controllers/ReplyController.php
+++ html/laravelapp/app/Http/Controllers/ReplyController.php
@@ -12,22 +12,36 @@ class ReplyController extends Controller
     protected $replyService;
     protected $utilService;
 
-    public function __construct(
-        ReplyService $replyService,
-        UtilService  $utilService
-    ) {
+    public function __construct(UtilService  $utilService)
+    {
         $this->replyService = $replyService;
-        $this->utilService  = $utilService;
     }
 
     public function create(ReplyCreatePost $request)
     {
-        return $this->replyService->create(
-            $request->thread_id,
-            Auth::id(),
-            $request->text,
-            $this->utilService->getIp()
-        );
+        $thread_id = $request->thread_id;
+        $user_id   = Auth::id();
+
+        if (!Thread::find($thread_id)) {
+            /* thread_id が存在しない */
+            $this->utilService->throwHttpResponseException("thread_id ${thread_id} は存在しません。");
+        }
+        if (!User::find($user_id)) {
+            /* user_id が存在しない */
+            $this->utilService->throwHttpResponseException("user_id ${user_id} は存在しません。");
+        }
+
+        /* number を取得する */
+        $number = Reply::where('thread_id', $thread_id)->count() + 1;
+
+        /* 作成して返却する */
+        return Reply::create([
+            'thread_id'  => $thread_id,
+            'number'     => $number,
+            'user_id'    => $user_id,
+            'text'       => $request->text,
+            'ip_address' => $this->utilService->getIp(),
+        ]);
     }
 
     public function selectAll()
--- html/laravelapp/app/Http/Controllers/ThreadController.php
+++ html/laravelapp/app/Http/Controllers/ThreadController.php
@@ -4,29 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ThreadCreatePost;
 use App\Models\Thread;
-use App\Services\ThreadService;
 use App\Services\UtilService;
 use Illuminate\Support\Facades\Auth;
 
 class ThreadController extends Controller
 {
-    protected $threadService;
     protected $utilService;
 
-    public function __construct(
-        ThreadService    $threadService,
-        UtilService      $utilService
-    ) {
-        $this->threadService    = $threadService;
-        $this->utilService      = $utilService;
+    public function __construct(UtilService $utilService)
+    {
+        $this->utilService = $utilService;
     }
 
     public function create(ThreadCreatePost $request)
     {
-        $user_id    = Auth::id();
-        $title      = $request->title;
-        $ip_address = $this->utilService->getIp();
-        return $this->threadService->create($user_id, $title, $ip_address);
+        return Thread::create([
+            'user_id'    => Auth::id(),
+            'title'      => $request->title,
+            'ip_address' => $this->utilService->getIp(),
+        ]);
     }
 
     public function selectAll()
