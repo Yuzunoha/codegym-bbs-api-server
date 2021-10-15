3bda6c54d5c3755f81cac0689b6d25ed40c22743
Yuzunoha <studysteadystandby@gmail.com>
2021-10-16 01:59:49 +0900

    /* per_pageの上限を設ける */

--- html/laravelapp/app/Http/Middleware/RequestFilter.php
+++ html/laravelapp/app/Http/Middleware/RequestFilter.php
@@ -19,6 +19,12 @@ class RequestFilter
         if ($request->per_page && ctype_digit($request->per_page)) {
             /* per_pageの指定があり、かつそれが十進数である */
             $per_page = intval($request->per_page);
+
+            $PER_PAGE_MAX_NUM = config('const')['PER_PAGE_MAX_NUM'];
+            if ($PER_PAGE_MAX_NUM < $per_page) {
+                /* per_pageの上限を設ける */
+                $per_page = $PER_PAGE_MAX_NUM;
+            }
         } else {
             /* per_pageの指定がない、またはあっても十進数でない */
             $per_page = 20;
--- html/laravelapp/config/const.php
+++ html/laravelapp/config/const.php
@@ -3,4 +3,5 @@
 return [
     'TITLE_MAX_LENGTH' => 64,
     'TEXT_MAX_LENGTH' => 1024,
+    'PER_PAGE_MAX_NUM' => 100,
 ];
