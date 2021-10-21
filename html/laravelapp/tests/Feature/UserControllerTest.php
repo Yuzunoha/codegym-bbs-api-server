ed2030a30b953c2327bdaa61bef14736c9575756
Yuzunoha <studysteadystandby@gmail.com>
2021-10-21 23:28:28 +0900

    異常系

--- html/laravelapp/tests/Feature/UserControllerTest.php
+++ html/laravelapp/tests/Feature/UserControllerTest.php
@@ -18,7 +18,7 @@ class UserControllerTest extends TestCase
         $this->新規登録してトークンを取得する();
     }
 
-    public function test_新規登録とloginと1件取得()
+    public function test_正常_新規登録とloginと1件取得()
     {
         $input = ['name' => 'a', 'email' => 'a@a.com', 'password' => 'a',];
         $expected = $this->post('/register', $input)->getData();
@@ -31,6 +31,31 @@ class UserControllerTest extends TestCase
         foreach (['id', 'name', 'email'] as $key) {
             $this->assertEquals($expected->$key, $actual->$key);
         }
+    }
+
+    public function test_異常_login_emailがDBに無い()
+    {
+        $input = ['name' => 'a', 'email' => 'a@a.com', 'password' => 'a',];
+        $this->post('/register', $input);
+        $response = $this->post('/login', [
+            'email' => 'a@A.com',
+            'password' => 'a',
+        ]);
+        $response->assertStatus(400);
+        $actual = $response->getData();
+        $this->assertEquals($actual->status, 400);
+        $this->assertEquals($actual->message, 'emailとpasswordの組み合わせが不正です。');
+    }
+
+    public function test_異常_register_email重複()
+    {
+        $input = ['name' => 'a', 'email' => 'a@a.com', 'password' => 'a',];
+        $this->post('/register', $input);
+        $response = $this->post('/register', $input);
+        $response->assertStatus(400);
+        $actual = $response->getData();
+        $this->assertEquals($actual->status, 400);
+        $this->assertEquals($actual->message, "email {$input['email']} は既に登録されています。");
     }
 
     public function test_一覧取得()
