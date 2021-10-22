d6fe9275a43a52812b25fef73d39cbcdbe949d44
Yuzunoha <studysteadystandby@gmail.com>
2021-10-22 15:07:08 +0900

    いじった

--- html/laravelapp/tests/Feature/UserControllerTest.php
+++ html/laravelapp/tests/Feature/UserControllerTest.php
@@ -2,15 +2,10 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class UserControllerTest extends TestCase
 {
-    use RefreshDatabase;
-
-    protected $token;
-
     public function setUp(): void
     {
         parent::setUp();
