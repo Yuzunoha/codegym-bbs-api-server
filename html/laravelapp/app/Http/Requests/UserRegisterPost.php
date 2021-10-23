a789fe2cccd51c7f4a45142edf1beeae48d07c4a
Yuzunoha <studysteadystandby@gmail.com>
2021-10-24 00:17:57 +0900

    ミスった名前の文字数縛り忘れてた

--- html/laravelapp/app/Http/Requests/UserRegisterPost.php
+++ html/laravelapp/app/Http/Requests/UserRegisterPost.php
@@ -7,7 +7,7 @@ class UserRegisterPost extends FormRequestBase
     public function rules()
     {
         return [
-            'name'     => 'required',
+            'name'     => 'required|string|max:' . config('const')['NAME_MAX_LENGTH'],
             'email'    => 'required|email',
             'password' => 'required',
         ];
