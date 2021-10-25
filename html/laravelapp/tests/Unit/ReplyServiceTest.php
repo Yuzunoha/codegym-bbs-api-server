cb31f57cc0bb1baf4b6acb1b164fe568ba0d6623
Yuzunoha <studysteadystandby@gmail.com>
2021-10-25 17:14:14 +0900

    異常系

--- html/laravelapp/tests/Unit/ReplyServiceTest.php
+++ html/laravelapp/tests/Unit/ReplyServiceTest.php
@@ -6,6 +6,7 @@ use App\Models\Reply;
 use App\Models\Thread;
 use App\Services\ReplyService;
 use App\Services\UtilService;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Tests\TestCase;
 
 class ReplyServiceTest extends TestCase
@@ -47,5 +48,39 @@ class ReplyServiceTest extends TestCase
         $actual = $replyService->create(2, 1, 'テキスト');
         $expected = Reply::with('user')->find(1);
         $this->assertEquals($expected, $actual);
+    }
+
+    public function test_異常系_create_スレッドが無い()
+    {
+        $replyService = new ReplyService(new UtilService);
+        $this->insertTestData();
+        $thread_id = 4;
+        try {
+            $replyService->create($thread_id, 1, 'テキスト');
+        } catch (HttpResponseException $e) {
+            $expected = json_encode([
+                'status' => 400,
+                'message' => "thread_id {$thread_id} は存在しません。",
+            ]);
+            $actual = json_encode($e->getResponse()->original);
+            $this->assertEquals($expected, $actual);
+        }
+    }
+
+    public function test_異常系_create_ユーザが無い()
+    {
+        $replyService = new ReplyService(new UtilService);
+        $this->insertTestData();
+        $user_id = 2;
+        try {
+            $replyService->create(1, $user_id, 'テキスト');
+        } catch (HttpResponseException $e) {
+            $expected = json_encode([
+                'status' => 400,
+                'message' => "user_id {$user_id} は存在しません。",
+            ]);
+            $actual = json_encode($e->getResponse()->original);
+            $this->assertEquals($expected, $actual);
+        }
     }
 }
