dd13f6795f5c7931e78d16c95eea673e7394c210
Yuzunoha <studysteadystandby@gmail.com>
2021-10-25 18:05:25 +0900

    引っ越した

--- html/laravelapp/app/Http/Controllers/ReplyController.php
+++ html/laravelapp/app/Http/Controllers/ReplyController.php
@@ -42,47 +42,25 @@ class ReplyController extends Controller
         );
     }
 
-    /**
-     * リプライの存在と、投稿主を調べる。
-     * エラーであれば例外を投げる。例外を投げなければvalidということ。
-     */
-    protected function checkExistAndOwnReply(int $reply_id, int $user_id): void
-    {
-        $reply = Reply::find($reply_id);
-        if (!$reply) {
-            /* 投稿が存在しない */
-            $this->utilService->throwHttpResponseException("reply_id ${reply_id} は存在しません。");
-        }
-        if (intval($user_id) !== intval($reply->user_id)) {
-            /* 自分の投稿でない */
-            $this->utilService->throwHttpResponseException("他のユーザの投稿は編集できません。");
-        }
-    }
-
     public function deleteOwnReply(ReplyDelete $request)
     {
-        $reply_id = $request->id;
-        $this->checkExistAndOwnReply($reply_id, Auth::id());
-
-        Reply::find($reply_id)->delete();
-        return [
-            'message' => "reply_id ${reply_id} のリプライを削除しました。",
-        ];
+        return $this->replyService->deleteOwnReply(
+            Auth::id(),
+            $request->id
+        );
     }
 
     public function updateOwnReply(ReplyPatch $request)
     {
-        $reply_id = $request->id;
-        $this->checkExistAndOwnReply($reply_id, Auth::id());
-
-        Reply::find($reply_id)->update([
-            'text' => $request->text,
-        ]);
-        return Reply::with('user')->find($reply_id);
+        return $this->replyService->updateOwnReply(
+            Auth::id(),
+            $request->id,
+            $request->text
+        );
     }
 
     public function selectById($id)
     {
-        return Reply::with('user')->find($id);
+        return $this->replyService->selectById($id);
     }
 }
--- html/laravelapp/app/Services/ReplyService.php
+++ html/laravelapp/app/Services/ReplyService.php
@@ -52,4 +52,44 @@ class ReplyService
             ->orderBy('number', 'desc')
             ->paginate($per_page);
     }
+
+    /**
+     * リプライの存在と、投稿主を調べる。
+     * エラーであれば例外を投げる。例外を投げなければvalidということ。
+     */
+    public function checkExistAndOwnReply(int $reply_id, int $user_id): void
+    {
+        $reply = Reply::find($reply_id);
+        if (!$reply) {
+            /* 投稿が存在しない */
+            $this->utilService->throwHttpResponseException("reply_id ${reply_id} は存在しません。");
+        }
+        if (intval($user_id) !== intval($reply->user_id)) {
+            /* 自分の投稿でない */
+            $this->utilService->throwHttpResponseException("他のユーザの投稿は編集できません。");
+        }
+    }
+
+    public function deleteOwnReply($user_id, $reply_id)
+    {
+        $this->checkExistAndOwnReply($reply_id, $user_id);
+        Reply::find($reply_id)->delete();
+        return [
+            'message' => "reply_id ${reply_id} のリプライを削除しました。",
+        ];
+    }
+
+    public function updateOwnReply($user_id, $reply_id, $text)
+    {
+        $this->checkExistAndOwnReply($reply_id, $user_id);
+        Reply::find($reply_id)->update([
+            'text' => $text,
+        ]);
+        return Reply::with('user')->find($reply_id);
+    }
+
+    public function selectById($id)
+    {
+        return Reply::with('user')->find($id);
+    }
 }
