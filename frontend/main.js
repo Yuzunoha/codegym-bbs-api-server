085cdac7575b42c5cddb31b60808cf8b9ecc90a3
Yuzunoha <studysteadystandby@gmail.com>
2021-10-29 13:29:48 +0900

    token撮れた

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,19 @@
 'use strict';
 
 const p = console.log;
+const host = 'http://localhost';
 
 loginSubmit.onclick = () => {
-  p(loginEmail.value);
+  fetch(`${host}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: loginEmail.value,
+      password: loginPassword.value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 };
